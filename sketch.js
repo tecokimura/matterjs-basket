e4a7b59172a33fc4a61871f9622f677192aa9ce9
Kimura Kazuyuki <kimura@KIMURAK-M4.local>
2020-06-30 16:07:50 +0900

    height * 2

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ let slingshot;
 let goal = [];
 
 function setup() {
-  const canvas = createCanvas(720, 720);
+  const canvas = createCanvas(720, 1440);
   engine = Engine.create();
   world = engine.world;
 
@@ -33,11 +33,13 @@ function setup() {
     boxesB[i] = new Box(500, 500 - i * 200, 40, 80);
   }
 
-  ball = new Ball(150, 550, 32);
+  ball = new Ball(150, 1250, 32);
 
-  slingshot = new SlingShot(150, 550, ball.body);
-  goal[0] = new Goal(400, 500);
-  goal[1] = new Goal(640, 350);
+  slingshot = new SlingShot(150, 1250, ball.body);
+  goal[0] = new Goal(400, 1000);
+  goal[1] = new Goal(640,  750);
+  goal[2] = new Goal(240, 1250);
+  goal[3] = new Goal(540, 1150);
 
 
   const mouse = Mouse.create(canvas.elt);
@@ -61,7 +63,7 @@ function checkReset() {
   var absBallVelo = Math.abs(ball.body.velocity.x)+Math.abs(ball.body.velocity.y);
   
   if (!slingshot.isAttached()
-  &&  absBallVelo * 4 < 0.05) {
+  &&  absBallVelo * 2 < 0.05) {
     reset();
   }
 
@@ -73,7 +75,7 @@ function checkReset() {
 
 function reset() {
     World.remove(world, ball.body);
-    ball = new Ball(150, 550, 32);
+    ball = new Ball(150, 1250, 32);
     slingshot.attach(ball.body);    
 }
 
@@ -96,7 +98,7 @@ function mouseReleased() {
     Matter.Body.scale(ball.body, 0.25, 0.25);
     setTimeout(() => {
       slingshot.fly();
-    }, 10);
+    }, 50);
 
   }
 }
@@ -104,7 +106,7 @@ function mouseReleased() {
 function renewBall() {
   if (!slingshot.isAttached()) {
     World.remove(world, ball.body);
-    ball = new Ball(150, 550, 32);
+    ball = new Ball(150, 1250, 32);
     slingshot.attach(ball.body);
   }
 }
@@ -125,7 +127,7 @@ function draw() {
   slingshot.show();
   ball.show();
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 4; i++) {
     goal[i].show();
   }
 }
