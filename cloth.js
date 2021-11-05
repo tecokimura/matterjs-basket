123746610953845a7c708e9d7f4b3985a3f25e55
Kimura Kaz <kimura@tecotec.local>
2021-11-05 17:36:31 +0900

    programming cloth.show()

--- cloth.js
+++ cloth.js
@@ -1,10 +1,14 @@
 class Cloth {
+
     constructor(xx, yy, columns, rows, columnGap, rowGap, crossBrace, particleRadius, particleOptions, constraintOptions) {
         var Body = Matter.Body,
         Bodies = Matter.Bodies,
         Common = Matter.Common,
         Composites = Matter.Composites;
     
+        this.columns = columns;
+        this.rows = rows;
+
         var group = Body.nextGroup(true);    
         var particleOptions = Common.extend({ inertia: Infinity, friction: 0.00001, collisionFilter: { group: group }, render: { visible: false }}, particleOptions);
         var constraintOptions = Common.extend({ stiffness: 0.06, render: { type: 'line', anchors: false } }, constraintOptions);
@@ -18,10 +22,13 @@ class Cloth {
         this.cloth.label = 'Cloth Body';
 
         Matter.World.add(world, this.cloth);
+
+        console.log(this.cloth);
     }
 
     show() {
-
+        this.cloth.Bodies
+        
     }
 
     
