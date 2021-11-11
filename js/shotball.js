0e0d3dcbc0e544c78baf876713b2191be5b15a77
Kimura Kaz <kimura@tecotec.local>
2021-11-11 17:43:22 +0900

    一旦オッケーな気がする！！ fix #5

--- js/shotball.js
+++ js/shotball.js
@@ -12,9 +12,18 @@ class Shotball {
     this.body = Matter.Bodies.circle(x, y, r/2, options);
     Matter.Body.setMass(this.body, 100);
     Matter.World.add(world, this.body);
+    
+    this.initX = x;
+    this.initY = y;
     this.r = r;
 
     this.image = image;
+  }
+
+  reset() {
+    Matter.Body.setPosition(this.body, { x: this.initX, y: this.initY})
+    Matter.Body.setVelocity(this.body, { x: 0, y: 0})
+    console.log(this.body);
   }
 
   // ボールの描画
@@ -22,9 +31,13 @@ class Shotball {
     const pos = this.body.position;
     const angle = this.body.angle;
     
+    // console.log("pX="+this.body.position.x+" Y="+this.body.position.y);
     push();
+    translate(pos.x, pos.y);
+    rotate(angle);
     imageMode(CENTER);
-    image(this.image, pos.x, pos.y, this.r, this.r);
+    image(this.image, 0, 0, this.r, this.r);
+    translate(0, 0);
     pop();
   }
 
